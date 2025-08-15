637eebadac126c133b3b31a890ced574bf3a6836
A41K <arokandris@gmail.com>
2025-08-15 20:07:08 +0200

    Remaking

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,5 @@ const { defineConfig } = require("eslint/config");
 const raycastConfig = require("@raycast/eslint-config");
 
 module.exports = defineConfig([
-  {
-    files: ["src/**/*.ts", "src/**/*.tsx"],
-    languageOptions: {
-      parserOptions: {
-        project: ["./tsconfig.json"],
-      },
-    },
-  },
   ...raycastConfig,
 ]);
