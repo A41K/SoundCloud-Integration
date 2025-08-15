cf5d013200350b769d503655716d5ecfb421a940
A41K <arokandris@gmail.com>
2025-08-15 16:18:01 +0200

    Yes sir

--- src/open.tsx
+++ src/open.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function Command() {
   useEffect(() => {
-    open("https://soundcloud.com/discover");
+    open("tidal://");
   },
  []);
 
--- src/play.tsx
+++ src/play.tsx
@@ -1,17 +1,10 @@
-import { showToast, Toast, open } from "@raycast/api";
+import { open } from "@raycast/api";
 import { useEffect } from "react";
 
 export default function Command() {
   useEffect(() => {
-    async function playSong() {
-      try {
-        await open("https://soundcloud.com");
-        await showToast(Toast.Style.Success, "Success", "Playing song on SoundCloud");
-      } catch (error) {
-        await showToast(Toast.Style.Failure, "Failed", error instanceof Error ? error.message : "Could not play song");
-      }
-    }
-    playSong();
+
+    open("tidal://play");
   }, []);
 
   return null;
